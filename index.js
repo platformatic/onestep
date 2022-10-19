07778b55680257d7343f4124df5ea442aaf33df5
Ivan Tymoshenko <bugagashenkj@gmail.com>
2022-10-19 12:19:37 +0300

    Use requestId to get application url

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 
 const { join } = require('path')
 const { createReadStream } = require('fs')
+const { setTimeout } = require('timers/promises')
 
 const core = require('@actions/core')
 const tar = require('tar')
 const { request } = require('undici')
 
 // TODO: replace with static URLs when ready
-const S3_SERVER_URL = core.getInput('platformatic-server-url')
-const COMPENDIUM_URL = core.getInput('platformatic-server-url')
-const GETAWAY_URL = core.getInput('platformatic-server-url')
+const SERVER_URL = core.getInput('platformatic-server-url')
 
 async function archiveProject (pathToProject, archivePath) {
   const options = { gzip: false, file: archivePath, cwd: pathToProject }
@@ -18,8 +17,8 @@ async function archiveProject (pathToProject, archivePath) {
 }
 
 async function uploadFile (apiKey, filePath) {
-  const url = S3_SERVER_URL + '/upload'
-  const { statusCode } = await request(url, {
+  const url = SERVER_URL + '/upload'
+  const { statusCode, body } = await request(url, {
     method: 'POST',
     headers: {
       authorization: `Bearer ${apiKey}`,
@@ -32,31 +31,18 @@ async function uploadFile (apiKey, filePath) {
   if (statusCode !== 200) {
     throw new Error(`Server responded with ${statusCode}`)
   }
+
+  const { requestId } = await body.json()
+  return requestId
 }
 
-async function createNewBucket (apiKey) {
-  const url = COMPENDIUM_URL + '/bucket'
-  const { statusCode, body } = await request(url, {
-    method: 'POST',
-    headers: {
-      authorization: `Bearer ${apiKey}`,
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({})
-  })
-
-  if (statusCode !== 200) {
-    throw new Error(`Server responded with ${statusCode}`)
-  }
-
-  const { id } = await body.json()
-  return id
-}
-
-async function getServerUrl (apiKey) {
-  const url = GETAWAY_URL + '/url'
+async function getResponseByReqId (apiKey, requestId) {
+  const url = SERVER_URL + '/url'
   const { statusCode, body } = await request(url, {
     method: 'GET',
+    query: {
+      request_id: requestId
+    },
     headers: {
       authorization: `Bearer ${apiKey}`
     }
@@ -66,8 +52,7 @@ async function getServerUrl (apiKey) {
     throw new Error(`Server responded with ${statusCode}`)
   }
 
-  const { url: applicationUrl } = await body.json()
-  return applicationUrl
+  return await body.json()
 }
 
 async function run () {
@@ -78,14 +63,30 @@ async function run () {
     core.info('Project has been successfully archived')
 
     const platformaticApiKey = core.getInput('platformatic-api-key')
-    await uploadFile(platformaticApiKey, archivePath)
+    const requestId = await uploadFile(platformaticApiKey, archivePath)
     core.info('Project has been successfully uploaded')
+    core.info('Creating Platformatic DB application, request ID: ' + requestId)
 
-    const bucketId = await createNewBucket(platformaticApiKey)
-    core.info(`New bucket has been created with id ${bucketId}`)
+    let applicationUrl = null
+    while (applicationUrl === null) {
+      const response = await getResponseByReqId(platformaticApiKey, requestId)
+      switch (response.status) {
+        case 'pending':
+          core.info('Application is not ready yet, waiting...')
+          await setTimeout(1000)
+          break
+        case 'ready':
+          applicationUrl = response.applicationUrl
+          break
+        case 'error':
+          throw new Error('Application creation failed: ', response.error)
+        default:
+          throw new Error('Unknown response status: ', response.status)
+      }
+    }
 
-    const applicationUrl = await getServerUrl(platformaticApiKey)
-    core.info(`Your application is available at ${applicationUrl}`)
+    core.info('Application has been successfully created')
+    core.info('Application URL: ' + applicationUrl)
 
     core.setOutput('platformatic-app-url', applicationUrl)
   } catch (error) {
