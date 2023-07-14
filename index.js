5f7a1e15d0ce544b8b02a4dfe2ea764c60ab6906
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-07-14 14:41:04 +0200

    feat: install dependencies dynamically

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict'
 
+const { execSync } = require('node:child_process')
+
+function installDependencies () {
+  console.log('Installing platformatic dependencies...')
+  try {
+    const result = execSync('npm install --omit=dev', {
+      cwd: __dirname,
+      timeout: 2 * 60 * 1000
+    })
+    console.log(result.toString())
+  } catch (error) {
+    console.error('Failed to install platformatic dependencies.', error)
+    process.exit(1)
+  }
+}
+
+installDependencies()
+
 require('./action.js')()
