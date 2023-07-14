fe1c7a29153d337408133e63ce1a4c69968bd022
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-07-14 14:41:04 +0200

    feat: install dependencies dynamically

--- node_modules/fastify-openapi-glue/lib/templates/standaloneJS/instructions.js
+++
@@ -1,9 +0,0 @@
-export default () => `
-In this directory use:
-+ "npm install" to install its dependencies
-+ "npm start" to start fastify using fastify-cli
-+ "npm run dev" to start fastify using fastify-cli with logging to the console
-+ "npm test" to run tests
-
-note: the auto generated test scaffolding does not contain any data yet !
-`;
