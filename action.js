0fe9ac3c407ad62edab3d0dd4d452e3757190fda
Matteo Collina <hello@matteocollina.com>
2023-10-11 14:54:22 +0100

    logging
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- action.js
+++ action.js
@@ -68,6 +68,8 @@ async function getGithubMetadata (octokit, isPullRequest) {
   const repositoryMetadata = getRepositoryMetadata()
   const branchMetadata = getBranchMetadata()
 
+  console.log(JSON.stringify(github.context, null, 2))
+
   const commitSha = isPullRequest
     ? github.context.payload.pull_request.head.sha
     : github.context.payload.head_commit.id
